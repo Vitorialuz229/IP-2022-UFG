816e690e4555482aa4cd186eeedb5b9d463c0d85
vitorialuz229 <vitorialuz831@gmail.com>
2023-09-19 18:18:51 -0300

    Expressões

--- Lista 1 - Expressões Matemáticas/Valor-em-Notas-e-Moedas.c
+++ Lista 1 - Expressões Matemáticas/Valor-em-Notas-e-Moedas.c
@@ -6,10 +6,16 @@
 #include <stdio.h>
 
 void main() { 
-    int NotaCem, NotaCinq, NotaDez, Valor, Moeda;    
-    scanf("%d", &Valor);
+    int NotaCem, NotaCinq, NotaDez, valor, Moeda;    
+    scanf("%d", &valor);
 
-    NotaCem 
+    NotaCem = valor/100; //NotaCem recebe o resultado da divisão
+    valor %= 100; //valor recebe o resto da divisão e armazena
+    NotaCinq = valor/50; NotaCinq; //recebe o resultado da divisão
+    valor %= 50; // valor recebe o resto da divisão e armazena
+    NotaDez = valor/10; //NotaDez recebe o resultado da divisão
+    valor %= 10; //valor recebe o resto da divisão e armazena
+    Moeda = valor;
 
     printf("NOTAS DE 100 = %d\n", NotaCem);
     printf("NOTAS DE 50 = %d\n", NotaCinq);
