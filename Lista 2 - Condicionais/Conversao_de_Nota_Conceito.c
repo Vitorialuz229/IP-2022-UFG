352f17da1ab69e2cf93d571e2b725e97ddd48917
vitorialuz229 <vitorialuz831@gmail.com>
2023-10-24 19:38:27 -0300

    Lista 2

--- Lista 2 - Condicionais/Conversao_de_Nota_Conceito.c
+++ Lista 2 - Condicionais/Conversao_de_Nota_Conceito.c
@@ -10,8 +10,22 @@ Intervalo de Nota: [9.0 a 10] - conceito A
                    [0 ,6,0)   -          D
 */
 
-void main () { 
-    float nota; 
-    scanf("%d", &nota);
+int main(){
+    double nota;
+ 
+    scanf("%lf", &nota);
+ 
+    if(nota >= 9.0 && nota <= 10.0)
+        printf("NOTA = %.1lf CONCEITO = A\n", nota);
+ 
+    else if(nota >= 7.5 && nota < 9.0)
+        printf("NOTA = %.1lf CONCEITO = B\n", nota);
+ 
+    else if(nota >= 6.0 && nota < 7.5)
+        printf("NOTA = %.1lf CONCEITO = C\n", nota);
+ 
+    else if(nota >= 0 && nota < 6.0)
+        printf("NOTA = %.1lf CONCEITO = D\n", nota);
 
-    
+return 0; 
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# IP-2022-01
+IP 2022-2
+
+OBS: As listas apresentam níveis. 
+LISTA 0 - ALGORITMO 
+Concentra estudo dirigidos divididos por área de estudo, com enfoque ajudar nas listas de exercicios. 
+
+LISTA 1 - EXPRESSÕES MATEMÁTICA 
+Concentra exercicios da lista propostos pelo professor que tem o objetivo ensinar lógica matématica. 
+
+LISTA 2 - CONDICIONAIS 
+Concentra exercicios da lista proposta pelo professor que tem o objetivo aplicar condicionais através probleminhas. 
